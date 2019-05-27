f1b705c05cce591115ee1e6e181e221d68338af1
codertimo <codertimo@gmail.com>
2019-05-27 18:41:10 +0900

    add intermediate layer

---
+++ model/encoder/intermediate.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+from model.utils import Dense, GELU
+
+
+class BERTIntermediate:
+    def __init__(self, config):
+        self.dense = Dense(config.hidden_size, config.intermediate_size)
+        self.activation = GELU()
+
+    @tf.function
+    def forward(self, hidden_states):
+        hidden_states = self.dense.forward(hidden_states)
+        hidden_states = self.activation.forward(hidden_states)
+        return hidden_states
+
